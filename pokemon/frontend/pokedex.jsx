c1f5b688d6c7c1def14e49239565a43a4d1d93c1
eric-to <erto@berkeley.edu>
2018-12-05 17:15:56 -0800

    Built api endpoint and setup redux cycle

--- pokemon/frontend/pokedex.jsx
+++ pokemon/frontend/pokedex.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as APIUtil from './util/api_util';
+import { receiveAllPokemon } from './actions/pokemon_actions';
+import configureStore from './store/store';
+
+window.fetchAllPokemon = APIUtil.fetchAllPokemon
+window.receiveAllPokemon = receiveAllPokemon
 
 document.addEventListener('DOMContentLoaded', () => {
+  const store = configureStore();
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
   const rootEl = document.getElementById('root');
   ReactDOM.render(<h1>Pokedex</h1>, rootEl);
 });
